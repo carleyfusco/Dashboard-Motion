1cedae252bff50ee9f129de483cf28cb3c2cda5d
carleyfusco <63366527+carleyfusco@users.noreply.github.com>
2021-06-06 15:26:52 -0400

    clip path

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -2,14 +2,11 @@ import { gsap } from "gsap";
 
 export let dashboardTL = gsap.timeline();
 
-dashboardTL.from("#DashboardBottom",{duration:1, y:1000})
+dashboardTL.from("#DashboardBottom",{duration:1, y:740},"sametime")
             .from("#WeatherLocationTopLeft",{duration:1, alpha:0},"sametime")
             .from("#TimeTopMiddle",{duration:1, alpha:0},"sametime")
 
             .from("#Range-1",{duration:3, drawSVG:0})
-            .from("#TickMarks",{duration:1, alpha:0},"sametime2")
-            .from("#TickMarkNumbers",{duration:1, alpha:0},"sametime2")
-           
 
             .from("#Bar1",{duration:.3, alpha:0})
             .from("#Bar2",{duration:.3, alpha:0})
@@ -19,28 +16,26 @@ dashboardTL.from("#DashboardBottom",{duration:1, y:1000})
             .from("#Bar6",{duration:.3, alpha:0})
             .from("#Bar7",{duration:.3, alpha:0})
 
-            .from("#Fill1",{duration:.2, alpha:0}, "sametime3")
-            .from("#Fill2",{duration:.2, alpha:0}, "sametime3")
-            .from("#Fill3",{duration:.2, alpha:0}, "sametime3")
-            .from("#Fill4",{duration:.2, alpha:0}, "sametime3")
-            .from("#Fill5",{duration:.2, alpha:0}, "sametime3")
-            .from("#GasNumbers",{duration:.2, alpha:0})
+            .from("#Fill1",{duration:1, alpha:0}, "sametime3")
+            .from("#Fill2",{duration:1, alpha:0}, "sametime3")
+            .from("#Fill3",{duration:1, alpha:0}, "sametime3")
+            .from("#Fill4",{duration:1, alpha:0}, "sametime3")
+            .from("#Fill5",{duration:1, alpha:0}, "sametime3")
 
-
-
+            .from("#TickMarks",{duration:1, alpha:0},"sametime3")
+            .from("#TickMarkNumbers",{duration:1, alpha:0},"sametime3")
+            .from("#GasNumbers",{duration:1, alpha:0},"sametime3")
+            .from("#Mph",{duration:1, alpha:0},"sametime3")
 
 
             .from("#RightOptionBoxes",{duration:1, x:1300})
 
             .from("#DashBoardWord",{duration:1, alpha:0})
-            .to("#NewDashBoardWord",{duration:1, alpha:0}, "sametime4")
-            .to("#Navigation",{duration:1, alpha:0}, "sametime4")
-            .to("#Music",{duration:1, alpha:0}, "sametime4")
+            .to("#NewDashBoardWord",{duration:.5, alpha:0}, "sametime4")
+            .to("#Navigation",{duration:.5, alpha:0}, "sametime4")
+            .to("#Music",{duration:.5, alpha:0}, "sametime4")
             .from("#DashBoardWord",{duration:1, x:307, y:22})
             .from("#DashboardElements",{duration:1, alpha:0})
             .from("#NavDashboard",{duration:1, y:1000})
             .from("#MusicDashboard",{duration:1, y:1000})
-            
-            .from("#Mph",{duration:1, alpha:0});
-
-
+            .from("#SpeedBar",{duration:6, x:-412});
--- dev/js/intro.js
+++ dev/js/intro.js
@@ -6,10 +6,10 @@ gsap.registerPlugin(DrawSVGPlugin);
 
 export let introTL = gsap.timeline();
 
-introTL.from("#Stroke-1",{duration:1.5, drawSVG:0})
-        .from("#Stroke-2",{duration:1.5, drawSVG:0})
-        .from("#Stroke-3",{duration:1.5, drawSVG:0})
-        .from("#Stroke-4",{duration:1.5, drawSVG:0})
+introTL.from("#Stroke-1",{duration:1, drawSVG:0})
+        .from("#Stroke-2",{duration:1, drawSVG:0})
+        .from("#Stroke-3",{duration:1, drawSVG:0})
+        .from("#Stroke-4",{duration:1, drawSVG:0})
         .to("#JeepOutline",{duration:1, alpha:0});
 
 
